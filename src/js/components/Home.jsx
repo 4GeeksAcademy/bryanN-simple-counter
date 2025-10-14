744405b58a031aa0dc7966fe33696a07cdf863ca
Bryan Navarro <diazbnavarro@gmail.com>
2025-10-14 04:12:12 +0000

    counter finished, still have to create cards type and import clock icon

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,10 +4,10 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<div className="text-center">
-            
+
 
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
@@ -21,6 +21,29 @@ const Home = () => {
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
 			</p>
+
+			<div className="container">
+				<div className="row">
+					<div className="col">
+						{props.digitSix}
+					</div>
+					<div className="col">
+						{props.digitFive}
+					</div>
+					<div className="col">
+						{props.digitFour}
+					</div>
+					<div className="col">
+						{props.digitThree}
+					</div>
+					<div className="col">
+						{props.digitTwo}
+					</div>
+					<div className="col">
+						{props.digitOne}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,36 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import Card from './components/Card';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+let root = ReactDOM.createRoot(document.getElementById('root'));
+let contador = 0
+
+setInterval(() => {
+  let one = Math.floor((contador / 1) % 10)
+  let two = Math.floor((contador / 10) % 10)
+  let three = Math.floor((contador / 100) % 10)
+  let four = Math.floor((contador / 1000) % 10)
+  let five = Math.floor((contador / 10000) % 10)
+  let six = Math.floor((contador / 100000) % 10)
+
+  contador++
+
+  root.render(
+    <React.StrictMode>
+      <Home
+        digitOne={one}
+        digitTwo={two}
+        digitThree={three}
+        digitFour={four}
+        digitFive={five}
+        digitSix={six}
+      />
+      <Card/>
+    </React.StrictMode>,
+  )
+
+
+}, 1000);
+
+
